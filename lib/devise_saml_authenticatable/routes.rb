96edb1145a051313f209931fc8546230aff1e9fe
Scott Serok <scott@serok.us>
2019-01-24 18:50:09 -0500

    saml logout path is overridden by standard logout path

--- lib/devise_saml_authenticatable/routes.rb
+++ lib/devise_saml_authenticatable/routes.rb
@@ -6,7 +6,7 @@ ActionDispatch::Routing::Mapper.class_eval do
       resource :session, only: [], controller: controllers[:saml_sessions], path: '' do
         get :new, path: 'saml/sign_in', as: "new_#{prefix}"
         post :create, path: 'saml/auth', as: prefix
-        match :destroy, path: mapping.path_names[:sign_out], as: "destroy_#{prefix}", via: mapping.sign_out_via
+        match :destroy, path: 'saml/logout', as: "destroy_#{prefix}", via: mapping.sign_out_via
         get :metadata, path: 'saml/metadata'
         match :idp_sign_out, path: 'saml/idp_sign_out', as: "idp_destroy_#{prefix}", via: [:get, :post]
       end
